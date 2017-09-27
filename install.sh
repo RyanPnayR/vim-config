cab8741a6cf62c391fbd1a6c42f92bb50d9fa616
Matthew Brock <mtbrock1@gmail.com>
2017-09-27 13:01:14 -0400

    Created install script

---
+++ install.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/env bash
+vim_dir=~/.vim
+config_dir=~/.config/vim
+backup_dir=$config_dir/backup
+
+maybe_do_backup() {
+  if ! [ -e $vim_dir ]; then
+    return 0
+  fi
+
+  read -r -p "Backup existing $vim_dir to $backup_dir? [y/n] "
+  if  ! [[ "$REPLY" =~ ^[Yy]$ ]]; then
+    skip_backup && return 0
+  fi
+
+  # Check if $vim_dir is symlink
+  if [ -L $vim_dir ]; then
+    # Show symlink info
+    echo "ls -lo $vim_dir"
+    ls -lo $vim_dir
+    # See if we still want to backup
+    read -r -p "$vim_dir is a symbolic link. Backup link target? [y/n] "
+    if  ! [[ "$REPLY" =~ ^[Yy]$ ]]; then
+      skip_backup && return 0
+    fi
+  fi
+
+  if do_backup ; then
+    echo "Backup complete" && return 0
+  else
+    fail "Could not complete backup."
+  fi
+}
+
+do_backup() {
+  # Do backup
+  backup_name=$(date +%Y-%m-%d_%s)
+  echo "Copying $vim_dir to $backup_dir/$backup_name"
+  mkdir -p $backup_dir
+  cp -RL $vim_dir $backup_dir/$backup_name
+}
+
+do_delete() {
+  [ -e $vim_dir ] && echo "Deleting $vim_dir"
+  rm -rf $vim_dir 2> /dev/null
+
+  if [ -e $vim_dir ]; then
+    fail "Could not delete existing $vim_dir"
+  fi
+}
+
+do_install() {
+  echo "Creating symlink"
+  ln -s $config_dir $vim_dir && return 0
+
+  # Failed to return
+  fail "Could not create symlink: ln -s $config_dir $vim_dir"
+}
+
+skip_backup() {
+  echo "Skipping backup of existing $vim_dir"
+}
+
+fail() {
+  echo "FAILURE: $1"; exit 1;
+}
+
+main() {
+  # Abort if we are already installed
+  [ -L $vim_dir ] && cd $vim_dir && realpath=$(pwd -P)
+  if [[ "$realpath" = "$config_dir" ]]; then
+    echo "Already installed." && exit 0
+  fi
+
+  # Ask for backup if necessary
+  # maybe_do_backup will fail if we tried to backup but couldn't
+  if maybe_do_backup ; then
+    do_delete
+    do_install
+    echo "Done"
+    exit 0
+  fi
+}
+
+main "$@"
